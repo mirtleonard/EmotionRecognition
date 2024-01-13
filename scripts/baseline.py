5c867c6d2762b2fc7bd824aa70f1c75cc604826b
mirtleonard <leonardmirt26@gmail.com>
2024-01-14 00:46:40 +0200

    init

--- scripts/baseline.py
+++ scripts/baseline.py
@@ -1,5 +1,6 @@
 import os
 import json
+import pickle
 import logging
 import argparse
 import warnings
@@ -48,7 +49,7 @@ def load_segments(segments_dir):
 
 def get_features(sig, sr, sigtype):
     if sigtype == 'bvp':
-        features = get_bvp_features(acquire_bvp(sig, sr), sr)
+        features = get_bvp_features(sig, sr)
     elif sigtype == 'eda':
         features = get_gsr_features(acquire_gsr(sig, sr, conversion=1e6), sr)
     elif sigtype == 'temp':
@@ -119,7 +120,7 @@ def get_data_rolling(segments, n, labeltype, majority):
 
 
 def get_data_discrete(segments, n, labeltype, majority):
-    X, y = {}, {}
+    X, y, z = {}, {}, {}
 
     # for each participant
     for pid, segs in segments.items():
@@ -127,7 +128,9 @@ def get_data_discrete(segments, n, labeltype, majority):
         pbar = tqdm(segs, desc=f'For subject {pid:02d}', ascii=True, dynamic_ncols=True)
 
         curr_X, curr_y, curr_segs = [], [], {}
+
         # for each segment
+        time = []
         for idx, signals, labels in pbar:
             # get labels and add to buffer
             s_a, s_v = int(labels[0]), int(labels[1])
@@ -176,20 +179,24 @@ def get_data_discrete(segments, n, labeltype, majority):
                 else:
                     a_val = curr_segs.pop('a')[-1]
                     v_val = curr_segs.pop('v')[-1]
-                
+
+                time.append(idx * 5)
                 curr_X.append(features)
                 if labeltype != 'sp':
                     curr_y.append([int(a_val > 2), int(v_val > 2)])
                 else:
                     curr_y.append([int(a_val > 5), int(v_val > 5)])
-
                 pbar.set_postfix({'processed': idx // n})
 
         # stack features for current participant and apply standardization
+        #if (pid == 32): 
+        #   print("Skipped subject 32")
+        #   continue;
         X[pid] = StandardScaler().fit_transform(np.stack(curr_X))
         y[pid] = np.stack(curr_y)
-
-    return X, y
+        z[pid] = time
+        
+    return X, y, z
 
 
 def prepare_kemocon(segments_dir, n, labeltype, majority, rolling):
@@ -200,9 +207,9 @@ def prepare_kemocon(segments_dir, n, labeltype, majority, rolling):
     if rolling:
         X, y = get_data_rolling(pid_to_segments, n, labeltype, majority)
     else:
-        X, y = get_data_discrete(pid_to_segments, n, labeltype, majority)
+        X, y, z  = get_data_discrete(pid_to_segments, n, labeltype, majority)
 
-    return X, y
+    return X, y, z
 
 
 # deprecated auroc and ap for compatibility with multiclass classification
@@ -231,42 +238,96 @@ def pred_random(y_classes, y_test, rng, ratios):
     #     probs = np.repeat(0.5, y_test.size)
     return get_results(y_test, preds)
 
+def load_model(model_name):
+    with open(model_name, 'rb') as f:
+        return pickle.load(f)
 
-def pred_gnb(X_train, y_train, X_test, y_test):
-    clf = GaussianNB().fit(X_train, y_train)
+def save_model(model, model_name):
+    with open(model_name, 'wb') as f:
+        pickle.dump(model, f)
+
+def pred_gnb(X_train, y_train, X_test, y_test, model_path):
+    clf = GaussianNB() 
+    if (os.path.exists(model_path)):
+        clf = load_model(model_path)
+    else: 
+        clf.fit(X_train, y_train)
     preds = clf.predict(X_test)
     # probs = clf.predict_proba(X_test)[:, 1]
+
+    save_model(clf, model_path)
+
     return get_results(y_test, preds)
 
+ploted = True
 
 def pred_xgb(X_train, y_train, X_test, y_test, seed, gpu, target):
     # load data into DMatrix
+    model_path = '../trained_models/xgb_test' + target + '.model'
+    #model_path = '../trained_models/xgb_new' + target + '.model'
     dtrain = xgb.DMatrix(X_train, label=y_train)
     dtest = xgb.DMatrix(X_test, label=y_test)
-
     # set parameters
     params = {
         'booster': 'gbtree',
         'verbosity': 1,
         'max_depth': 6,
-        'eta': 0.3,
+        'learning_rate': 0.3,
         'objective': 'multi:softmax',
         'eval_metric': 'mlogloss' if target == 'multiclass' else 'logloss',
         'num_class': 4 if target == 'multiclass' else 2,
         'seed': seed,
     }
 
-    # if gpu=True
-    if gpu:
-        params['gpu_id'] = 0
-        params['tree_method'] = 'gpu_hist'
-
     # train model and predict
     num_round = 100
-    bst = xgb.train(params, dtrain, num_round)
-    preds = bst.predict(dtest)
-    
+    xgb_clf = xgb.XGBClassifier(
+        n_estimators=num_round,
+        booster='gbtree',
+        verbosity= 1,
+        max_depth= 6,
+        learning_rate= 0.3,
+        objective= 'multi:softmax',
+        eval_metric= 'mlogloss' if target == 'multiclass' else 'logloss',
+        num_class= 4 if target == 'multiclass' else 2,
+        seed = seed, 
+        )
+
+    xgb_clf = xgb.train(params, dtrain, num_round)
+    if (os.path.exists(model_path)):
+        print("Loading model from: ", model_path)
+        xgb_clf = load_model(model_path)
+    #preds = bst.predict(dtest)
+
+    #xgb_clf.fit(X_train, y_train) 
+    #proba = xgb_clf.predict_proba(X_test)
+    #preds = xgb_clf.predict(X_test)
+    #plot prbabilities
+
+    preds = xgb_clf.predict(dtest)
+    #save the model
+    save_model(xgb_clf, model_path)
+
     # return results
+    return get_results(y_test, preds)
+    
+def plot_proba(proba, y_test):
+    import matplotlib.pyplot as plt
+    classes = ['la-lv', 'la-hv', 'ha-lv', 'ha-hv']
+    for i in range(4):
+        plt.plot(proba[:,i], label=classes[i])
+    plt.xlabel('Test samples')
+    plt.ylabel('Probability')
+    plt.legend()
+    plt.show()
+
+def pred_rforest(X_train, y_train, X_test, y_test, seed, target):
+    from sklearn.ensemble import RandomForestClassifier
+    clf = RandomForestClassifier(max_depth=2, random_state=0)
+    clf.fit(X_train, y_train)
+    preds = clf.predict(X_test)
+    # probs = clf.predict_proba(X_test)[:, 1]
+    print(get_results(y_test, preds))
     return get_results(y_test, preds)
 
 
@@ -279,7 +340,6 @@ def get_baseline_kfold(X, y, seed, target, n_splits, shuffle, gpu):
     X = np.concatenate(list(X.values()))
     y = np.concatenate(list(y.values()))
     logging.getLogger('default').info(f'Dataset size: {X.shape}')
-
     # get labels corresponding to target class
     if target == 'arousal':
         y = y[:, 0]
@@ -304,35 +364,41 @@ def get_baseline_kfold(X, y, seed, target, n_splits, shuffle, gpu):
             'Random': pred_random(y_classes, y_test, rng, ratios=np.repeat(1/n_classes, n_classes)),
             'Majority': pred_majority(majority, y_test),
             'Class ratio': pred_random(y_classes, y_test, rng, ratios=class_ratios),
-            'Gaussian NB': pred_gnb(X_train, y_train, X_test, y_test),
+            'Gaussian NB': pred_gnb(X_train, y_train, X_test, y_test, '../trained_models/gnb_' + target + '.model'),
             'XGBoost': pred_xgb(X_train, y_train, X_test, y_test, seed, gpu, target),
+            'Rforest': pred_rforest(X_train, y_train, X_test, y_test, seed, target),
         }
-
     # return results as table
     results = {(fold, classifier): values for (fold, _results) in results.items() for (classifier, values) in _results.items()}
     results_table = pd.DataFrame.from_dict(results, orient='index').stack().unstack(level=1).rename_axis(['Fold', 'Metric'])
-    return results_table[['Random', 'Majority', 'Class ratio', 'Gaussian NB', 'XGBoost']]
+    return results_table[['Random', 'Majority', 'Class ratio', 'Gaussian NB', 'XGBoost', 'Rforest']]
 
 
 def get_baseline_loso(X, y, seed, target, n_splits, shuffle, gpu):
     # initialize random number generator
     rng = default_rng(seed)
 
+    classes = np.unique(np.concatenate(list(y.values())), axis=0).tolist()
+
     results = {}
     # for each participant split train & test
     for pid in X.keys():
         X_train, X_test = np.concatenate([v for k, v in X.items() if k != pid]), X[pid]
         y_train, y_test = np.concatenate([v for k, v in y.items() if k != pid]), y[pid]
-
         # get labels corresponding to target class
         if target == 'arousal':
             y_train, y_test = y_train[:, 0], y_test[:, 0]
         elif target == 'valence':
             y_train, y_test = y_train[:, 1], y_test[:, 1]
+        elif target == 'multiclass': 
+            y_train = np.fromiter(map(lambda x: classes.index(x.tolist()), y_train), dtype=np.int)
+            y_test = np.fromiter(map(lambda x: classes.index(x.tolist()), y_test), dtype=np.int)
+
 
         # skip current user if there aren't both labels (0, 1) in the test set
-        if len(Counter(y_test)) != 2:
-            continue
+        #print(len(y_test), len(X_test), len(y_train), len(X_train))
+        #if len(Counter(y_test)) != 2:
+        #    continue
 
         # get majority label and class ratios
         y_classes, y_counts = np.unique(y_train, return_counts=True)
@@ -341,16 +407,16 @@ def get_baseline_loso(X, y, seed, target, n_splits, shuffle, gpu):
 
         # get classification results
         results[pid] = {
-            'Random': pred_random(y_classes, y_test, rng),
+            'Random': pred_random(y_classes, y_test, rng, ratios=class_ratios),
             'Majority': pred_majority(majority, y_test),
             'Class ratio': pred_random(y_classes, y_test, rng, ratios=class_ratios),
-            'Gaussian NB': pred_gnb(X_train, y_train, X_test, y_test),
-            'XGBoost': pred_xgb(X_train, y_train, X_test, y_test, seed, gpu),
+            'Gaussian NB': pred_gnb(X_train, y_train, X_test, y_test, '../trained_models/gnb_' + target + '.model'),
+            'XGBoost': pred_xgb(X_train, y_train, X_test, y_test, seed, gpu, target),
+            'Rforest': pred_rforest(X_train, y_train, X_test, y_test, seed, target),
         }
-
     results = {(pid, classifier): value for (pid, _results) in results.items() for (classifier, value) in _results.items()}
     results_table = pd.DataFrame.from_dict(results, orient='index').stack().unstack(level=1)
-    return results_table[['Random', 'Majority', 'Class ratio', 'Gaussian NB', 'XGBoost']]
+    return results_table[['Random', 'Majority', 'Class ratio', 'Gaussian NB', 'XGBoost', 'Rforest']]
 
 
 def get_baseline(X, y, seed, target, cv, n_splits, shuffle, gpu):
@@ -361,6 +427,17 @@ def get_baseline(X, y, seed, target, cv, n_splits, shuffle, gpu):
 
     return results
 
+def write_features_to_file(features, labels, time):
+    classes = np.unique(np.concatenate(list(labels.values())), axis=0).tolist()
+    #print(labels)
+    dtypes = [('pid', int), ('segs', int), ('labels', int), ('time', int)]
+    for pid, segs in features.items():
+        y = np.fromiter(map(lambda x: classes.index(x.tolist()), labels[pid]), dtype=np.int)
+        data = np.c_[segs, y, time[pid]]
+
+        print("Writing files for subject: ", pid) 
+        file = '../../features/' + str(pid) + '.csv';
+        np.savetxt(file, data, delimiter=",")
 
 if __name__ == "__main__":
     # initialize parser
@@ -369,9 +446,9 @@ if __name__ == "__main__":
     parser.add_argument('-tz', '--timezone', type=str, default='UTC', help='a pytz timezone string for logger, default is UTC')
     parser.add_argument('-s', '--seed', type=int, default=0, help='seed for random number generation, default is 0')
     parser.add_argument('-t', '--target', type=str, default='valence', help='target label for classification, must be either "valence" or "arousal"')
-    parser.add_argument('-l', '--length', type=int, default=5, help='number of consecutive 5s-signals in one segment, default is 5')
+    parser.add_argument('-l', '--length', type=int, default=1, help='number of consecutive 5s-signals in one segment, default is 5')
     parser.add_argument('-y', '--label', type=str, default='s', help='type of label to use for classification, must be either "s"=self, "p"=partner, "e"=external, or "sp"=self+partner (default="s")')
-    parser.add_argument('--majority', default=False, action='store_true', help='set majority label for segments, default is last')
+    parser.add_argument('--majority', default=True, action='store_true', help='set majority label for segments, default is last')
     parser.add_argument('--rolling', default=False, action='store_true', help='get segments with rolling: e.g., s1=[0:n], s2=[1:n+1], ..., default is no rolling: e.g., s1=[0:n], s2=[n:2n], ...')
     parser.add_argument('--cv', type=str, default='kfold', help='type of cross-validation to perform, must be either "kfold" or "loso"')
     parser.add_argument('--splits', type=int, default=4, help='number of folds for k-fold stratified classification, default is 4')
@@ -383,7 +460,7 @@ if __name__ == "__main__":
     # check commandline arguments
     if args.target not in ['valence', 'arousal', 'multiclass']:
         raise ValueError(f'--target must be either "valence" or "arousal" or "multiclass", but given {args.target}')
-    elif args.length < 5:
+    elif args.length < 1:
         raise ValueError(f'--length must be greater than 5')
     elif args.label not in ['s', 'p', 'e', 'sp']:
         raise ValueError(f'--label must be either "s", "p", "e", or "sp", but given {args.label}')
@@ -402,7 +479,8 @@ if __name__ == "__main__":
     # get features and labels
     segments_dir = os.path.expanduser(args.root)
     logger.info(f'Processing segments from {segments_dir}, with: seed={args.seed}, target={args.target}, length={args.length*5}s, label={args.label}, majority={args.majority}, rolling={args.rolling}, cv={args.cv}, splits={args.splits}, shuffle={args.shuffle}, gpu={args.gpu}')
-    features, labels = prepare_kemocon(segments_dir, args.length, args.label, args.majority, args.rolling)
+    features, labels, time = prepare_kemocon(segments_dir, args.length, args.label, args.majority, args.rolling)
+    #write_features_to_file(features, labels, time)
     logger.info('Processing complete.')
 
     # get classification results
